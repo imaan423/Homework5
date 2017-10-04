0b8541769229891bbfd8021b01efb12460cd36c3
Imaan Munir <imunir@umich.edu>
2017-10-03 20:57:23 -0400

    adding homework 5

---
+++ homework5.py
@@ -0,0 +1,13 @@
+import re
+filename = open('regex_sum_37794.txt')
+list1 = []
+for number in filename:
+    find = re.findall('[0-9]+',number)
+    list1.extend(find)
+
+
+total = [int(i) for i in list1]
+final = sum(total)
+print (final)
+
+
